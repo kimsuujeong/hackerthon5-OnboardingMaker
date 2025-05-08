65fe97be647ed1e1bfe946f676d08b513f869f7c
sujeong <thehapy0103@naver.com>
2025-05-08 17:36:51 +0900

    GroupEntity 변수명 camelCase 변경

--- backend/src/main/java/com/example/onboarding/missionWrite/Entity/GroupEntity.java
+++ backend/src/main/java/com/example/onboarding/missionWrite/Entity/GroupEntity.java
@@ -18,37 +18,37 @@ public class GroupEntity {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "group_id")
-    private int group_id;
+    private int groupId;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "mission_id", nullable = false)
-    private Mission mission_id;
+    private Mission missionId;
 
     @Column(name = "group_num", nullable = false)
-    private int group_num;
+    private int groupNum;
 
 	public int getGroup_id() {
-		return group_id;
+		return groupId;
 	}
 
 	public void setGroup_id(int group_id) {
-		this.group_id = group_id;
+		this.groupId = group_id;
 	}
 
 	public Mission getMission_id() {
-		return mission_id;
+		return missionId;
 	}
 
 	public void setMission_id(Mission mission_id) {
-		this.mission_id = mission_id;
+		this.missionId = mission_id;
 	}
 
 	public int getGroup_num() {
-		return group_num;
+		return groupNum;
 	}
 
 	public void setGroup_num(int group_num) {
-		this.group_num = group_num;
+		this.groupNum = group_num;
 	}
     
 }
