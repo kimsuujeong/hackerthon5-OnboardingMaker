39060d9336912b9e4c1ce14ddd7e2fee69cba778
Clsz <45088680+ClszK@users.noreply.github.com>
2025-05-08 13:26:18 +0900

    Feat: 임시 로그인 페이지

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
-=======
-<<<<<<< HEAD
-import { useState } from "react";
-import "./App.css";
-import LoginPage from "./pages/LoginPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,26 +14,3 @@ function App() {
 }
 
 export default App;
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
->>>>>>> de6e4b5a9c297912a6a5a5cf44cbd21ce9122013
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <LoginPage />
-    </>
-  );
-}
-
-<<<<<<< HEAD
-export default App;
-=======
-export default App
->>>>>>> origin/main
->>>>>>> de6e4b5a9c297912a6a5a5cf44cbd21ce9122013
