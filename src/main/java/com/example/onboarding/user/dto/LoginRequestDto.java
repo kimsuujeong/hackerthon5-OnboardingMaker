5a81d24a1467624ceff4ee2fe7a84a96f7aec790
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-05-08 10:41:04 +0900

    feat: 회원가입, 로그인, 미션 조회 API 구현

---
+++ src/main/java/com/example/onboarding/user/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.onboarding.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/example/onboarding/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.onboarding.user.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 100)
+    private String nickname;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean isAdmin;
+}
