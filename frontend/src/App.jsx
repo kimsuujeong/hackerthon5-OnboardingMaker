1e37d9fe39f73a4c30eeb257599e1f6ecd5f8e53
LeeHyunKyeong <115594388+LeeHyunKyeong@users.noreply.github.com>
2025-05-08 16:57:25 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        하이
-      </div>
-=======
       <Login/>
->>>>>>> d88eb31d96d2fae7c7e37dedb08aa9ff02392dc9
     </>
   );
 }
