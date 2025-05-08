2aa1decb720ff0e91255ef42ab0005b68210e4d3
LeeHyunKyeong <115594388+LeeHyunKyeong@users.noreply.github.com>
2025-05-08 19:51:31 +0900

    Add ReplyPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import LoginForm from "@/pages/LoginPage.jsx";
 import MainPage from "./pages/MainPage";
+import ReplyPage from "./pages/ReplyPage";
 
 function App() {
   return (
     <>
       <LoginForm />
       <MainPage/>
+      <ReplyPage />
     </>
   );
 }
