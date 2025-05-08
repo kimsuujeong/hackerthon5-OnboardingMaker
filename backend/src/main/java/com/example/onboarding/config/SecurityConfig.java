0cdc46daaa653e74862b21d21fd613bed83441d1
Clsz <45088680+ClszK@users.noreply.github.com>
2025-05-08 14:10:10 +0900

    [Merge]Clean branch (#11)
    * test
    
    * Env: "backend 디렉토리 생성"
    
    * Rename: "최상위 Root로 README 옮김"
    
    * Env: "컨테이너화 파일"
    
    최초 구현부터 docker-compose를 활용해서 로컬 환경에서 쉽게 개발하려고 했으나,
    docker build 시간이 생각보다 너무 많이 잡아먹고
    Front의 hot-loading(코드 변경 후 바로 적용)이 안되서 마지막 배포단계에 구현할 예정
    
    * Env: "Frontend 초기 프로젝트"
    
    * Fix: rootProject 이름 변경
    
    * Feat: 임시 로그인 페이지 생성
    
    * Fix: 충돌 text 그대로 입력한 부분 삭제
    
    * Fix: 왜 안돼?????

--- backend/src/main/java/com/example/onboarding/config/SecurityConfig.java
+++ backend/src/main/java/com/example/onboarding/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.onboarding.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,23 +8,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/user/register", "/user/login").permitAll()
-                .anyRequest().authenticated();
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeHttpRequests()
+        .requestMatchers("/user/register", "/user/login").permitAll()
+        .anyRequest().authenticated();
 
-        return http.build();
-    }
+    return http.build();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
